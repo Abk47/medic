8059fc6dae007ec3627ee144cc5cf67ff6a95a45
Abuka Nshunju - IT - LIFE <abuka.nshunju@jubileetanzania.com>
2020-08-12 11:39:20 +0300

    Add names for form input

--- resources/views/forms/info.blade.php
+++ resources/views/forms/info.blade.php
@@ -14,73 +14,73 @@
               <div class="form-group row">
                  <label for="company" class="col-sm-2 col-form-label">Employer's Name</label>
                  <div class="col-sm-10">
-                    <input type="text" readonly class="form-control" id="company" value="Jubilee Insurance Company">
+                    <input type="text" readonly class="form-control" name="company" value="Jubilee Insurance Company">
                  </div>
               </div>
               <div class="form-group row">
                  <label for="Surname" class="col-sm-2 col-form-label">Member's Surname</label>
                  <div class="col-sm-4">
-                    <input type="text" class="form-control" id="userSurname" placeholder="Enter your surname here" required>
+                    <input type="text" class="form-control" name="userSurname" placeholder="Enter your surname here" required>
                  </div>
                  <label for="Surname" class="col-sm-2 col-form-label">Other Names</label>
                  <div class="col-sm-4">
-                    <input type="text" class="form-control" id="otherNames" required>
+                    <input type="text" class="form-control" name="otherNames" required>
                  </div>
               </div>
               <div class="form-group row">
                  <label for="DOB"class='col-sm-2 col-form-label'>Date of Birth</label>
                  <div class="col-sm-10">
-                    <input type="date" class="form-control" id="DOB" required>
+                    <input type="date" class="form-control" name="DOB" required>
                  </div>
               </div>
               <div class="form-group row">
                  <label for="passport"class='col-sm-2 col-form-label'>ID or Passport number</label>
                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="passport" required>
+                    <input type="text" class="form-control" name="passport" required>
                  </div>
               </div>
               <div class="form-group row">
                  <label for="occupation"class='col-sm-2 col-form-label'>Occupation</label>
                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="occupation" required>
+                    <input type="text" class="form-control" name="occupation" required>
                     <small id="occupation" class="form-text text-muted">If more than one, state all.</small>
                  </div>
               </div>
               <div class="form-group row">
                  <label for="address" class='col-sm-2 col-form-label'>Postal Address</label>
                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="address" required>
+                    <input type="text" class="form-control" name="address" required>
                  </div>
               </div>
               <div class="form-group row">
                  <label for="location" class='col-sm-2 col-form-label'>Physical location of place of work</label>
                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="location" placeholder="Building/Street" required>
+                    <input type="text" class="form-control" name="location" placeholder="Building/Street" required>
                  </div>
               </div>
               <div class="form-group row">
                  <label for="home" class='col-sm-2 col-form-label'>Physical home address</label>
                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="home" placeholder="Residence/Area/House No." required>
+                    <input type="text" class="form-control" name="home" placeholder="Residence/Area/House No." required>
                  </div>
               </div>
               <div class="form-group row">
                  <label for="telephone" class=" col-sm-2 col col-form-label">Telephone</label>
                  <div class="col">
-                    <input type="text" class="form-control" placeholder="Office" id="office">
+                    <input type="text" class="form-control" placeholder="Office" name="office">
                  </div>
                  <div class="col">
-                    <input type="text" class="form-control" placeholder="House" id="house">
+                    <input type="text" class="form-control" placeholder="House" name="house">
                  </div>
                  <div class="col">
-                    <input type="text" class="form-control" placeholder="Mobile" id="mobile" required>
+                    <input type="text" class="form-control" placeholder="Mobile" name="mobile" required>
                  </div>
               </div>
               <div class="form-group row">
                  <label for="email" class='col-sm-2 col-form-label'>Email address</label>
                  <div class="col-sm-10">
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <input type="email" class="form-control" name="usermail" aria-describedby="emailHelp" placeholder="Enter email" required>
+                    <small id="usermail" class="form-text text-muted">We'll never share your email with anyone else.</small>
                  </div>
               </div>
            </fieldset>
