07a37fcb93def1cb6296654d720e878692cd8958
Abuka Nshunju - IT - LIFE <abuka.nshunju@jubileetanzania.com>
2020-08-25 20:01:16 +0300

    Minor dashboard url fixes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/user/dashboard', 'AdminController@index')->name('dashboard');
+Route::get('/user/dashboard/{user}', 'AdminController@index')->name('show.dashboard');
 
 Route::get('/contacts', 'ContactController@show')->name('contacts');
 
