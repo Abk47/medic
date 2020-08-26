a149d981f6b102d896e5c021b500fe41e81bfb9d
Abuka Nshunju - IT - LIFE <abuka.nshunju@jubileetanzania.com>
2020-08-26 20:33:41 +0300

    Add model fillable details

--- app/Member.php
+++ app/Member.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'company',
+        'surname',
+        'other_names',
+        'user_email',
+        'DOB',
+        'passport',
+        'home_location',
+        'work_location',
+        'occupation',
+        'address',
+        'house_no',
+        'mobile_no',
+        'user_email',
+        'office_no'
+        ];
 }
