bb7a84f726aed5858ef352778d8324c97335b7ef
Abuka Nshunju - IT - LIFE <abuka.nshunju@jubileetanzania.com>
2020-09-07 10:41:47 +0300

    Add user relationship

---
+++ app/Confidential.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Confidential extends Model
+{
+    protected $fillable = [
+        ''
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
