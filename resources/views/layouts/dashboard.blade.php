860c9e1e28c181a5e162d79025fa3938e3a402ca
Abuka Nshunju - IT - LIFE <abuka.nshunju@jubileetanzania.com>
2020-09-17 15:45:58 +0300

    Change form status

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -142,7 +142,7 @@
               @if(count($status) === 1)
               <p>
                 Status
-                <span class="right badge badge-warning">Pending</span>
+                <span class="right badge badge-warning">Submitted</span>
               </p>
               @else
               <p>
